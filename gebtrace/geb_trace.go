052e9382890c4c0bf38e6d51c140785848c41dd8
Adam Kiss <masterada@gmail.com>
2018-03-19 13:27:01 +0000

    Resolve "Add trace start middlewares (gin+geb)"

--- gebtrace/geb_trace.go
+++ gebtrace/geb_trace.go
@@ -8,6 +8,13 @@ import (
 	"gitlab.com/proemergotech/trace-go"
 )
 
+type settings struct {
+	start  bool
+	corGen func() *trace.Correlation
+}
+
+type Option func(*settings)
+
 // PublishMiddleware return middleware which will extract the trace context from event headers and starts a new child span.
 // If there's no parent context, it will start a new root span and adds the 'span.missing' tag to the span.
 // It will also add correlation related tags.
@@ -70,9 +77,15 @@ func PublishMiddleware(tracer opentracing.Tracer, logger trace.Logger) geb.Middl
 
 // OnEventMiddleware return middleware which will extract the trace context from event headers and starts a new child span.
 // If there's no parent context, it will start a new root span and adds the 'span.missing' tag to the span.
+// If the Start option is passed to the method, it will start a new root span, without adding the 'span.missing' tag.
 // It will also add correlation related tags.
 // If an error happens or one of the middleware panics, it will mark the span as failed and continue panicking.
-func OnEventMiddleware(tracer opentracing.Tracer, logger trace.Logger) geb.Middleware {
+func OnEventMiddleware(tracer opentracing.Tracer, logger trace.Logger, options ...Option) geb.Middleware {
+	s := &settings{}
+	for _, opt := range options {
+		opt(s)
+	}
+
 	return func(e *geb.Event, next func(*geb.Event) error) (err error) {
 		ctx := e.Context()
 		h := e.Headers()
@@ -81,19 +94,15 @@ func OnEventMiddleware(tracer opentracing.Tracer, logger trace.Logger) geb.Middl
 			CorrelationID: h[trace.CorrelationIDField],
 			WorkflowID:    h[trace.WorkflowIDField],
 		}
+		if cor.CorrelationID == "" && s.start {
+			cor = s.corGen()
+		}
 		ctx = trace.WithCorrelation(ctx, cor)
 
 		msg := "GEB in: " + e.EventName()
 		opts := []opentracing.StartSpanOption{ext.SpanKindConsumer}
-		if spanCtx, err := tracer.Extract(opentracing.TextMap, opentracing.TextMapCarrier(h)); err != nil {
-			err = errors.Wrap(err, "No trace: "+msg)
-			logger.Error(ctx, err.Error(), "error", err)
-
-			opts = append(opts, opentracing.Tag{
-				Key:   trace.SpanMissingTag,
-				Value: true,
-			})
-		} else {
+		spanCtx, err := tracer.Extract(opentracing.TextMap, opentracing.TextMapCarrier(h))
+		if err == nil {
 			opts = append(opts, opentracing.FollowsFrom(spanCtx))
 		}
 
@@ -102,6 +111,17 @@ func OnEventMiddleware(tracer opentracing.Tracer, logger trace.Logger) geb.Middl
 			opts...,
 		)
 		defer span.Finish()
+
+		if s.start && err == nil {
+			err = errors.New("Trace found, ignoring Start: "+msg)
+			logger.Error(ctx, err.Error(), "error", err)
+			trace.Error(span, err)
+		} else if !s.start && err != nil{
+			err = errors.Wrap(err, "No trace: "+msg)
+			logger.Error(ctx, err.Error(), "error", err)
+			span.SetTag(trace.SpanMissingTag, true)
+			trace.Error(span, err)
+		}
 
 		trace.AddCorrelationTags(span, cor)
 
@@ -122,5 +142,15 @@ func OnEventMiddleware(tracer opentracing.Tracer, logger trace.Logger) geb.Middl
 		}()
 
 		return next(e)
+	}
+}
+
+// Start option can be passed to OnEventMiddleware to start the tracing
+// instead of following it from a previous trace based on the geb headers.
+// A generator function is required for generation the Correlation object when starting a trace.
+func Start(gen func() *trace.Correlation) Option {
+	return func(opts *settings) {
+		opts.start = true
+		opts.corGen = gen
 	}
 }
--- gintrace/gin_trace.go
+++ gintrace/gin_trace.go
@@ -8,11 +8,24 @@ import (
 	"gitlab.com/proemergotech/trace-go"
 )
 
+type settings struct {
+	start  bool
+	corGen func() *trace.Correlation
+}
+
+type Option func(*settings)
+
 // Middleware return middleware which will extract the trace context from headers and starts a new child span.
 // If there's no parent context, it will start a new root span and adds the 'span.missing' tag to the span.
+// If the Start option is passed to the method, it will start a new root span, without adding the 'span.missing' tag.
 // It will also add correlation and http related tags, like the http method, status code etc..
 // If an error happens or one of the middleware panics, it will mark the span as failed and continue panicking.
-func Middleware(tracer opentracing.Tracer, logger trace.Logger) gin.HandlerFunc {
+func Middleware(tracer opentracing.Tracer, logger trace.Logger, options ...Option) gin.HandlerFunc {
+	s := &settings{}
+	for _, opt := range options {
+		opt(s)
+	}
+
 	return func(gCtx *gin.Context) {
 		req := gCtx.Request
 		ctx := req.Context()
@@ -22,19 +35,15 @@ func Middleware(tracer opentracing.Tracer, logger trace.Logger) gin.HandlerFunc 
 			CorrelationID: h.Get(trace.CorrelationIDHeader),
 			WorkflowID:    h.Get(trace.WorkflowIDHeader),
 		}
+		if cor.CorrelationID == "" && s.start {
+			cor = s.corGen()
+		}
 		ctx = trace.WithCorrelation(ctx, cor)
 
 		msg := "HTTP in: [" + req.Method + "] " + req.URL.Path
 		opts := []opentracing.StartSpanOption{ext.SpanKindConsumer}
-		if spanCtx, err := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(h)); err != nil {
-			err = errors.Wrap(err, "No trace: "+msg)
-			logger.Error(ctx, err.Error(), "error", err)
-
-			opts = append(opts, opentracing.Tag{
-				Key:   trace.SpanMissingTag,
-				Value: true,
-			})
-		} else {
+		spanCtx, err := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(h))
+		if err == nil {
 			opts = append(opts, opentracing.ChildOf(spanCtx))
 		}
 
@@ -43,6 +52,17 @@ func Middleware(tracer opentracing.Tracer, logger trace.Logger) gin.HandlerFunc 
 			opts...,
 		)
 		defer span.Finish()
+
+		if s.start && err == nil {
+			err = errors.New("Trace found, ignoring Start: "+msg)
+			logger.Error(ctx, err.Error(), "error", err)
+			trace.Error(span, err)
+		} else if !s.start && err != nil{
+			err = errors.Wrap(err, "No trace: "+msg)
+			logger.Error(ctx, err.Error(), "error", err)
+			span.SetTag(trace.SpanMissingTag, true)
+			trace.Error(span, err)
+		}
 
 		trace.AddCorrelationTags(span, cor)
 		ext.HTTPMethod.Set(span, req.Method)
@@ -66,5 +86,15 @@ func Middleware(tracer opentracing.Tracer, logger trace.Logger) gin.HandlerFunc 
 		}()
 
 		gCtx.Next()
+	}
+}
+
+// Start option can be passed to Middleware to start the tracing
+// instead of following it from a previous trace based on the http headers.
+// A generator function is required for generation the Correlation object when starting a trace.
+func Start(gen func() *trace.Correlation) Option {
+	return func(opts *settings) {
+		opts.start = true
+		opts.corGen = gen
 	}
 }
